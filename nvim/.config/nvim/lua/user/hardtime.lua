272e132782d916ceccde0b7b84c5867e146d7f28
Micah Shlain <mcshlain@gmail.com>
2024-01-30 11:19:16 +0200

    fix hardtime config

--- nvim/.config/nvim/lua/user/hardtime.lua
+++ nvim/.config/nvim/lua/user/hardtime.lua
@@ -1,11 +1,16 @@
 local M = {
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 }
 
-
 M.opts = {
-  max_count = 9999
+  max_count = 9999,
+  disabled_keys = {
+    ["<Up>"] = { "n", "x" },
+    ["<Down>"] = { "n", "x" },
+    ["<Left>"] = { "n", "x" },
+    ["<Right>"] = { "n", "x" },
+  },
 }
 
 return M

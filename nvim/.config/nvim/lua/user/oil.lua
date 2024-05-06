c26c790874c96e2be8dea0974c11a7ddf1680cc2
Micah Shlain <mcshlain@gmail.com>
2024-05-06 11:13:30 +0300

    added oil plugin

---
+++ nvim/.config/nvim/lua/user/oil.lua
@@ -0,0 +1,10 @@
+local M = {
+  "stevearc/oil.nvim"
+}
+
+function M.config()
+  require("oil").setup()
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+end
+
+return M

4752cfa65e2fe090031096c0eacae8650a7d3a07
Micah Shlain <mcshlain@gmail.com>
2026-03-11 11:28:28 +0200

    changed nvim theme to monokai

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,11 +1,15 @@
-local M = { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 }
+-- local M = { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 }
+local M = { "git@github.com:tanvirtin/monokai.nvim.git", name = "monokai", priority = 1000 }
+
 function M.config()
-  vim.cmd.colorscheme "moonfly"
+  -- vim.cmd.colorscheme "moonfly"
+  vim.cmd.colorscheme "monokai"
 
   vim.cmd "hi clear SpellBad"
   vim.cmd "hi SpellBad ctermfg=red guifg=#r31a1c gui=undercurl guisp=#3182bd"
   vim.cmd "hi clear ColorColumn"
   vim.cmd "hi ColorColumn gui=bold"
+  vim.cmd "hi Normal guibg=#000000"
 end
 
 return M

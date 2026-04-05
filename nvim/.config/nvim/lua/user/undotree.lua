b117717960f7a78d4be8ab5d2361f2845df1e2c8
Micah Shlain <mcshlain@gmail.com>
2026-04-05 15:02:16 +0300

    atone as undotree plugin

--- nvim/.config/nvim/lua/user/undotree.lua
+++ nvim/.config/nvim/lua/user/undotree.lua
@@ -1,57 +1,36 @@
--- local M = { "mbbill/undotree" }
---
---
--- function M.config()
---   local wk = require "which-key"
---
---   wk.add {
---     { "<leader>u", group = "Undo" },
---     { "<leader>uu", "<cmd>:UndotreeToggle<cr>", desc = "Undo Tree", remap = false },
---   }
--- end
---
--- return M
-
 local M = {
-  "debugloop/telescope-undo.nvim",
-  dependencies = { -- note how they're inverted to above example
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
-  },
-  -- keys = {
-  --   { -- lazy style key map
-  --     "<leader>u",
-  --     "<cmd>Telescope undo<cr>",
-  --     desc = "undo history",
-  --   },
-  -- },
-  opts = {
-    extensions = {
-      undo = {
-        side_by_side = true,
-        layout_strategy = "vertical",
-        layout_config = {
-          preview_height = 0.8,
-        },
-      },
-    },
-  },
+  "XXiaoA/atone.nvim",
+  event = "VeryLazy",
 }
 
-M.config = function(_, opts)
-  -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-  -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-  -- defaults, as well as each extension).
-  require("telescope").setup(opts)
-  require("telescope").load_extension "undo"
+function M.config()
+  require("atone").setup {
+    layout = { direction = "right" },
+    auto_attach = {
+      enabled = true,
+      excluded_ft = { "oil" },
+    },
+    ui = { border = "rounded" },
+    keymaps = { tree = { undo_to = "u" } },
+  }
+
+  -- Map <CR> in the atone tree to undo to the selected node and close the panel.
+  -- undo_to is remapped to "u" above so we can feed it without recursion.
+  -- defer_fn ensures close() runs after atone's internal scheduled callbacks finish.
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "atone",
+    callback = function(ev)
+      vim.keymap.set("n", "<CR>", function()
+        vim.api.nvim_feedkeys("u", "m", false)
+        vim.defer_fn(function() require("atone.core").close() end, 10)
+      end, { buffer = ev.buf, nowait = true })
+    end,
+  })
 
   local wk = require "which-key"
 
   wk.add {
-    { "<leader>u", group = "Undo" },
-    { "<leader>uu", "<cmd>:lua require('telescope').extensions.undo.undo()<cr>", desc = "Undo Tree", remap = false },
+    { "<leader>u", "<cmd>Atone toggle<cr>", desc = "Undo Tree", remap = false },
   }
 end
 

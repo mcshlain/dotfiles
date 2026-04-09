98ca16a0f3750e904fb30b0452b50c29296b701a
Micah Shlain <mcshlain@gmail.com>
2026-04-09 15:14:38 +0300

    improved typescript styled-components highlighting

---
+++ nvim/.config/nvim/queries/typescript/injections.scm
@@ -0,0 +1,25 @@
+; inherits: ecma
+
+; styled(Component)<TypeArgs>`<css>`
+(call_expression
+  function: (non_null_expression
+    (instantiation_expression
+      (call_expression
+        function: (identifier) @_name
+        (#eq? @_name "styled"))))
+  arguments: ((template_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
+    (#set! injection.language "styled")))
+
+; styled.div<TypeArgs>`<css>`
+(call_expression
+  function: (non_null_expression
+    (instantiation_expression
+      (member_expression
+        object: (identifier) @_name
+        (#eq? @_name "styled"))))
+  arguments: ((template_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
+    (#set! injection.language "styled")))

56afa68a5ceb7b650b8033028ea6442086fe38ff
Micah Shlain <mcshlain@gmail.com>
2026-04-09 13:37:08 +0300

    use eslint for formatting typescript files

--- nvim/.config/nvim/lua/user/conform.lua
+++ nvim/.config/nvim/lua/user/conform.lua
@@ -5,13 +5,21 @@ local M = {
 
 function M.config()
   require("conform").setup {
+    formatters = {
+      eslint_d = {
+        command = function()
+          local local_bin = vim.fn.findfile("node_modules/.bin/eslint_d", vim.fn.getcwd() .. ";")
+          return local_bin ~= "" and local_bin or "eslint_d"
+        end,
+      },
+    },
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "ruff_organize_imports", "ruff_format" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
+      javascript = { "eslint_d", "prettier" },
+      typescript = { "eslint_d", "prettier" },
+      javascriptreact = { "eslint_d", "prettier" },
+      typescriptreact = { "eslint_d", "prettier" },
       css = { "prettier" },
       html = { "prettier" },
       json = { "prettier" },
